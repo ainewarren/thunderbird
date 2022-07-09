c502bd64b3f42e9d5ca6ba8d1dfbaf08c5cbb8fe
ainewarren <97672398+ainewarren@users.noreply.github.com>
2022-07-09 13:57:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
   function resize() {
     var w = window.innerWidth;
     console.log(w);
-    if (w >= 1400) {
-      $(".sunrise img").attr("src", "images/sunrise-2160.jpg");
+    if (w >= 1300) {
+      // $(".sunrise img").attr("src", "images/sunrise-2160.jpg");
       $(".info").addClass("info-large").removeClass("container-fluid").removeClass("p-3").addClass("p-5");
       $(".top-large").addClass("show");
       $(".nav").addClass("navbar-large").show();
@@ -13,16 +13,19 @@
       // $(".sunrise").addClass("sunrise-large");
       $(".title").addClass("title-large")
       $(".wrapper").addClass("wrapper-large");
-      $(".sunrise-large").show();
+      $(".sunrise-img").show();
       $(".sunrise").hide();
+      $(".dreamcatcher").hide();
       $(".drop").hide();
+      $("#about").addClass("about-large");
+
       // $("#therapy-large").removeClass("hide");
       // $(".nav-therapy").attr("href", "#therapy-large");
       // $("#therapy-small").hide();
     }
 
-    else if (w < 1400) {
-      $(".sunrise img").attr("src", "images/sunrise-1440.jpg");
+    else if (w < 1300) {
+      // $(".sunrise img").attr("src", "images/sunrise-1440.jpg");
       $(".info").removeClass("info-large").addClass("container-fluid").addClass("p-3");
       $(".top").removeClass("hide");
       $(".top-large").removeClass("show");
@@ -32,14 +35,29 @@
       // $(".sunrise").removeClass("sunrise-large");
       $(".title").removeClass("title-large")
       $(".wrapper").removeClass("wrapper-large");
-      $(".sunrise-large").hide();
+      $(".sunrise-img").hide();
       $(".sunrise").show();
       $(".drop").show();
+      $("#about").removeClass("about-large");
+      $(".dreamcatcher").show();
       // $("#therapy-large").addClass("hide");
       // $("#therapy-small").show();
       // $(".nav-therapy").attr("href", "#therapy-small");
     };
+
+    if (w > 1300 && w < 1400) {
+      $(".sunrise-img").attr("src", "images/sunrise-1440-tall.jpg");
+    }
+
+    else if (w >= 1400 && w < 1500) {
+    $(".sunrise-img").attr("src", "images/sunrise-1920.jpg");
+    }
+
+    else if (w >= 1500) {
+    $(".sunrise-img").attr("src", "images/sunrise-2160.jpg");
   };
+
+};
 
 resize();
 
@@ -54,6 +72,4 @@ $(window).on("load", function() {
 
 $(".drop").on("click", function() {
   $(".nav").toggle("slow");
-}
-
-)
+})
